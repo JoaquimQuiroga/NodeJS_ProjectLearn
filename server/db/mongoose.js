6acf3e95dd42afdf298fa471cbabe8dd4a6b9506
Joaquim Quiroga <joaquim.quiroga@phineal.com>
2018-03-20 09:17:49 -0300

    Add test for PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
-//mongoose.connect('mongodb://djo:1qsefth@ds117759.mlab.com:17759/node_playground')
+//mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://djo:1qsefth@ds117759.mlab.com:17759/node_playground')
 
 module.exports = {mongoose}
