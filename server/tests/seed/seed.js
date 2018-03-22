f3c3159a8c6716d10b3c472039bb98e45036f53e
Joaquim Quiroga <joaquim.quiroga@phineal.com>
2018-03-22 17:04:32 -0300

    ADD POST /users/login route

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken')
+
+const {ObjectID} = require('mongodb')
+
+const {Todo} = require('./../../models/todo')
+
+const {User} = require('./../../models/user')
+
+const userOneId = new ObjectID()
+const useTwoId = new ObjectID()
+const users = [{
+  _id: userOneId,
+  email: 'joaquim.quiroga@phineal.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: useTwoId,
+  email: 'joaquim.quiroga2@phineal.com',
+  password: 'userTwoPass',
+}]
+
+let dummyTodo = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo'
+}]
+
+const populatedummyTodo = (done) => {
+  Todo.remove({}).then(() => {
+    console.log('HERE2.5')
+    return Todo.insertMany(dummyTodo)
+    console.log('HERE3')
+  }).then(() => {
+    console.log('HERE4')
+    done()
+  })
+}
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    let userOne = new User(users[0]).save()
+    let userTwo = new User(users[1]).save()
+    console.log('HERE')
+    return Promise.all([userOne,userTwo])
+  }).then(() => {
+    console.log('HERE2')
+    done()
+  })
+}
+
+module.exports = {dummyTodo, populatedummyTodo, users, populateUsers}
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,22 +2,14 @@ const expect = require('expect')
 const request = require('supertest')
 const {ObjectID} = require('mongodb')
 
-const {app} = require('./server')
-const {Todo} = require('./models/todo')
+const {app} = require('./../server')
+const {Todo} = require('./../models/todo')
+const {User} = require('./../models/user')
 
-let dummyTodo = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo'
-}]
+const {dummyTodo, populatedummyTodo, users, populateUsers} = require('./seed/seed')
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(dummyTodo)
-  }).then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populatedummyTodo)
 
 describe('POST /todos', () => {
 
@@ -177,5 +169,85 @@ describe('PATCH /todos/:id', () => {
       }
       done()
     })
+  })
+})
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/user/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString())
+        expect(res.body.email).toBe(users[0].email)
+      })
+      .end(done)
+  })
+  it('should return 401 if not auth', (done) => {
+    request(app)
+      .get('/user/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({})
+      })
+      .end(done)
+  })
+
+})
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    let email = 'example@ecample.com'
+    let password = '123mnb!'
+
+
+    request(app)
+      .post('/users')
+      .send({email,password})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toBeDefined()
+        expect(res.body._id).toBeDefined()
+        expect(res.body.email).toBe(email)
+      })
+      .end((err)=>{
+        if(err){
+          return done(err)
+        }
+        User.findOne({email}).then((user)=>{
+          expect(user).toBeDefined()
+          expect(user.password).not.toBe(password)
+          done()
+        })
+      })
+  })
+
+  it('should return validation error if request invalid', (done) => {
+    let email = "joaquim.quiroga2phineal.com"
+    let password = '123m'
+    request(app)
+      .post('/users')
+      .send({email,password})
+      .expect(400)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).not.toBeDefined()
+      })
+      .end(done)
+
+  })
+
+  it('should not create a user if email already in use', (done) => {
+    let email = "joaquim.quiroga@phineal.com"
+    let password = '123mnb!'
+
+    request(app)
+      .post('/users')
+      .send({email,password})
+      .expect(400)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).not.toBeDefined()
+      })
+      .end(done)
   })
 })
